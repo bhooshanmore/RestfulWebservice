d9246f0a455d9946e9bdaabaa94e85f6cb92fd9f
bhooshanmore <bhooshanmore@gmail.com>
2019-02-19 21:38:38 +0530

    Spring Assing

---
+++ PriceReductionWebService/PriceReductionWebService/src/main/java/com/cts/controller/ProductPriceReducedController.java
@@ -0,0 +1,32 @@
+package com.cts.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cts.model.LabelTypeVo;
+import com.cts.model.ProductVo;
+import com.cts.service.PriceReducedProductService;
+
+@RestController
+
+public class ProductPriceReducedController {
+	
+	@Autowired
+	PriceReducedProductService priceReducedProductService;
+	
+	@GetMapping("/PriceReductionByCatId/{categoryId}")
+	public List<ProductVo> getPriceReducedProductByCategoryId(@PathVariable(required=true) Integer categoryId,
+											@RequestParam(required=false) LabelTypeVo priceLabelType){
+		
+		Optional<LabelTypeVo> labelType = Optional.ofNullable(priceLabelType);
+		
+		return priceReducedProductService.getPriceReducedProducts(categoryId, labelType);
+	}
+		
+}
